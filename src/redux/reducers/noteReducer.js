f3ffe22f8b627be6b332d57195740c9f6e1e5071
Rituraj Yadav <riturajy2002@gmail.com>
2023-08-25 17:27:07 +0530

    first_commit

---
+++ src/redux/reducers/noteReducer.js
@@ -0,0 +1,36 @@
+import { ADD_NOTE, DELETE_NOTE } from "../actions/noteActions";
+
+const initialState={
+    notes:[{text:"Lorem ispum dolor sit amit,c olumnd w"
+,createdOn:new Date()},
+{
+    text:"Arrtgfdyutyr  arstdghjtyr adfrtew"
+    ,createdOn:new Date()
+},
+]
+};
+export function noteReducer(state=initialState, action){
+
+    switch(action.type){
+        case ADD_NOTE:
+            return {
+                ...state,
+                notes:[
+                    ...state.notes,
+                    {
+                        text:action.text,
+                        createdOn: new Date()
+                    }
+                ]
+            }
+        case DELETE_NOTE:
+            state.notes.splice(action.index,1);
+            return{
+                ...state,
+                notes:[...state.notes]
+                }
+            
+        default:
+            return state;
+    }
+}
